288e9f6c1f3dc01663ea1e177fadda756ed73f51
林爱娣 <1433551474@qq.com>
2023-02-05 21:54:07 +0800

    feat: 移除图片压缩功能

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -11,12 +11,10 @@ import { configMockPlugin } from './mock';
 import { configCompressPlugin } from './compress';
 import { configStyleImportPlugin } from './styleImport';
 import { configThemePlugin } from './theme';
-import { configImageminPlugin } from './imagemin';
 import { configSvgIconsPlugin } from './svgSprite';
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
   const {
-    VITE_USE_IMAGEMIN,
     VITE_USE_MOCK,
     VITE_LEGACY,
     VITE_BUILD_COMPRESS,
@@ -61,9 +59,6 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
 
   // The following plugins only work in the production environment
   if (isBuild) {
-    // vite-plugin-imagemin
-    VITE_USE_IMAGEMIN && vitePlugins.push(configImageminPlugin());
-
     // rollup-plugin-gzip
     vitePlugins.push(
       configCompressPlugin(VITE_BUILD_COMPRESS, VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE),
